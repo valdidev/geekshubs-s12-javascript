b95a50d0b05377db2ab48f8bcd35d90c2e55f68a
valdidev <valdicode.fernando@gmail.com>
2022-10-05 13:14:15 +0200

    fin presentación vanilla JS

--- js/main.js
+++ js/main.js
@@ -38,8 +38,7 @@ let videojuegos = ["lost ark", "warframe", "lol", "wow", "metal slug"];
 let posibles = videojuegos.filter(juego => juego != "lol");
 console.log(posibles); */
 
-// KATA - Letra del DNI
-
+/* // KATA - Letra del DNI - validación real
 let numDNI = parseInt(prompt("Introduce el número de DNI"));
 let letraDNI = prompt("Introduce la letra del DNI").toUpperCase();
 const letrasDNI = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
@@ -48,9 +47,40 @@ if (letrasDNI[modulo] == letraDNI) {
     console.log("El DNI es verdadero");
 } else {
     console.log("El DNI es falso");
+} */
+
+/* // KATA - Sacar la letra del DNI
+let numDNI = parseInt(prompt("Introduce el número de DNI"));
+const letrasDNI = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
+let pos = numDNI % 23;
+
+console.log(numDNI,letrasDNI[pos]) */
+
+// ARRAY vs OBJETO
+let carlos = ["Carlos", 27, "Valencia", "videojuegos"];
+
+let alumnoCarlos = {
+    nombre: "Carlos",
+    edad: 27,
+    ciudad: "Valencia",
+    hobbie: ["videojuegos", "películas", "leer", "viajar"],
+    amigos: {
+        amigo1: "Jose",
+        amigo2: "Marta",
+        amigo3: {
+            nombreAmigo3: "Mara",
+            hobbiesAmigo3: {
+                hobbieMara1: "Leer",
+                hobbieMara2: "Pasear"
+            }
+        }
+    }
 }
 
-
+console.log(carlos[2]);
+console.log(alumnoCarlos);
+console.log(alumnoCarlos.ciudad);
+console.log(alumnoCarlos.amigos.amigo3);
 
 
 
